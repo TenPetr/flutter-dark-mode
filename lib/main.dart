2ab4a0cf9fbefec9c98fd1b5351105b7db374447
Petr Jelínek <petulinek.jel@gmail.com>
2020-06-24 14:01:43 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:dark_mode/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Dark Mode',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
